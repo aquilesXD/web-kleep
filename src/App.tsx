37f8aa4664e4f50fabf69381fb4e63c72186328e
aquilesXD <aquilesbarboza1989@gmail.com>
2025-04-01 11:53:23 -0400

    Hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ProfileGeneral from './pages/profile/ProfileGeneral';
 import ProfileConnectedAccounts from './pages/profile/ProfileConnectedAccounts';
 import ProfileSecurity from './pages/profile/ProfileSecurity';
